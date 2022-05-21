e8f32435ad9e089200f4d8af2edbcb7488c11ccf
Eduarda Moraes <99514703+moraespy@users.noreply.github.com>
2022-05-21 12:37:02 -0300

    Add files via upload

---
+++ LP - Copia/src/LP1a100.java
@@ -0,0 +1,19 @@
+package lp;
+
+public class LP1a100 {
+    public static void main(String args []){
+        
+        // for(int i = 0; i <= 100; i++){
+        //     if(i % 2 != 0){
+        //         System.out.println(i);
+
+    int i = 0;
+    while(i <=100){
+        i++;
+        if(i % 2 != 0){
+        System.out.println(i + ",");
+        }
+    }
+        
+    }
+}
